c41ec8136e47b1678cc7ca29d1508e222956d579
Nick Gundobin <ngundobin@gmail.com>
2021-02-04 13:18:40 -0500

    use other states

--- src/NumberGame.js
+++ src/NumberGame.js
@@ -3,16 +3,26 @@ import "./NumberGame.css";
 
 const NumberGame = (props) => {
 	const genRand = () => Math.floor(Math.random() * 10) + 1;
+	const restart = () => {
+		setTarget(genRand);
+		setGuess(0);
+		setGuessCount(0);
+	};
+	const makeGuess = () => {
+		setGuess(genRand());
+		setGuessCount(guessCount + 1);
+	};
 	const [guess, setGuess] = useState(genRand);
 	const [target, setTarget] = useState(genRand);
+	const [guessCount, setGuessCount] = useState(0);
 	const isWinner = target === guess;
 	return (
 		<div className="NumberGame">
 			<h1>Target Number: {target}</h1>
 			<h1 className={isWinner ? "winner" : "loser"}>Your guess: {guess}</h1>
-			{!isWinner && (
-				<button onClick={() => setGuess(genRand())}>Generate Num</button>
-			)}
+			<h4>Guess Number: #{guessCount}</h4>
+			{!isWinner && <button onClick={makeGuess}>Generate Num</button>}
+			<button onClick={restart}>New Game</button>
 		</div>
 	);
 };
